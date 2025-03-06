501bb04bba258c45ed3d87f0e3fce83fb448a8f0
Rosa1Salinas <r.salinas@coadvil.edu.co>
2025-03-06 16:44:15 -0500

    ejercicio 5.py

--- ejercicio 5.py
+++ ejercicio 5.py
@@ -1,3 +1,3 @@
-cm = int(input("Ingrese el número de centímetros: "))
+cm = int(input("Ingrese el número en centímetros: "))
 pulgadas = cm / 2.54
 print("El número de centímetros en pulgadas es: ", pulgadas)
